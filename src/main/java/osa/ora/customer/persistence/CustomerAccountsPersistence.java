d298efc8fb2c0d62272566b079dd3df4a39f8b53
ooransa <ooransa@OORANSA-QA.eg.oracle.com>
2020-01-01 12:53:03 +0300

    Fix table name

--- src/main/java/osa/ora/customer/persistence/CustomerAccountsPersistence.java
+++ src/main/java/osa/ora/customer/persistence/CustomerAccountsPersistence.java
@@ -21,7 +21,7 @@ public class CustomerAccountsPersistence {
 		return conn;
 	}
 	public JsonMessage save(Accounts account) {
-		String insertTableSQL = "INSERT INTO customer_account "
+		String insertTableSQL = "INSERT INTO customer_accounts "
 				+ "(customer_id, account_no, type) "
 				+ "VALUES(?,?,?)";
 
@@ -43,7 +43,7 @@ public class CustomerAccountsPersistence {
 	}
 
 	public JsonMessage delete(long id, String account_no) {
-		String deleteRowSQL = "DELETE FROM customer_account WHERE customer_id=? and account_no=?";
+		String deleteRowSQL = "DELETE FROM customer_accounts WHERE customer_id=? and account_no=?";
 		try (PreparedStatement preparedStatement = getConnection()
 				.prepareStatement(deleteRowSQL)) {
 			preparedStatement.setLong(1, id);
@@ -61,12 +61,12 @@ public class CustomerAccountsPersistence {
 	}
 
 	public Accounts[] findAll() {
-		String queryStr = "SELECT * FROM customer_account";
+		String queryStr = "SELECT * FROM customer_accounts";
 		return this.query(queryStr);
 	}
 
 	public Accounts[] findbyId(long id) {
-		String queryStr = "SELECT * FROM customer_account WHERE customer_id=" + id;
+		String queryStr = "SELECT * FROM customer_accounts WHERE customer_id=" + id;
 		Accounts accounts[] = this.query(queryStr);
 		return accounts;
 	}
