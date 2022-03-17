728b08fa7c8da6d7d7e3249e0fb8c035b2bfd07a
Osama Oransa <osa_ora@yahoo.com>
2022-03-17 11:52:45 +0200

    Update init-serverless.sh

--- configs/init-serverless.sh
+++ configs/init-serverless.sh
@@ -13,16 +13,7 @@ echo "Service 'CustomerData-db' deployed successfully as ephemeral"
 echo "Login to CustomerData-db mysql pod and install the schema using:"
 echo "mysql -u root"
 echo "connect customers"
-echo "CREATE TABLE `customers`.`customer` (`id` INT NOT NULL AUTO_INCREMENT, `firstName` VARCHAR(45) NULL, `lastName` VARCHAR(45) NULL, `email` VARCHAR(45) NULL, `city` VARCHAR(45) NULL, `state` VARCHAR(45) NULL,`birthday` VARCHAR(45) NULL, PRIMARY KEY (`id`));"
-
-echo "INSERT INTO `customers`.`customer` (`id`, `firstName`, `lastName`, `email`, `city`,`state`, `birthday`) VALUES ('1', 'Osama', 'Oransa', 'osa.ora@acme.com', 'Cairo','No', '2000-01-09');
-echo "INSERT INTO `customers`.`customer` (`id`, `firstName`, `lastName`, `email`, `city`,`state`, `birthday`) VALUES ('2', 'Sameh', 'Ahmed', 'sa.or@acme.com', 'Dubai','Yes', '2001-02-20');
-
-echo "CREATE TABLE `customers`.`customer_accounts` (`customer_id` INT NOT NULL,`account_no` VARCHAR(45) NOT NULL, `type` VARCHAR(45) NULL,PRIMARY KEY (`customer_id`, `account_no`));"
-
-echo "INSERT INTO `customers`.`customer_accounts` (`customer_id`, `account_no`, `type`) VALUES ('1', '123456-1', '100');"
-echo "INSERT INTO `customers`.`customer_accounts` (`customer_id`, `account_no`, `type`) VALUES ('1', '123456-2', '101');"
-echo "INSERT INTO `customers`.`customer_accounts` (`customer_id`, `account_no`, `type`) VALUES ('2', '2323445-1', '100');"
+curl https://raw.githubusercontent.com/osa-ora/CustomerDataService/master/script/initial_customer_schema.sql
 
 oc apply -f https://raw.githubusercontent.com/osa-ora/CustomerDataService/master/configs/image.yaml
 oc apply -f https://raw.githubusercontent.com/osa-ora/CustomerDataService/master/configs/build-config.yaml
